90a6af66a9deecf3c13d40e88efc0a3e7f0fb710
yofreke <sparkyjoe94@aim.com>
2015-06-01 11:26:50 -0700

    Workaround #840
    See https://github.com/boto/boto/issues/840

--- boto/s3/key.py
+++ boto/s3/key.py
@@ -940,7 +940,8 @@ class Key(object):
             if size is not None:
                 kwargs['size'] = size
             headers['_sha256'] = compute_hash(**kwargs)[0]
-        headers['Expect'] = '100-Continue'
+        # See: https://github.com/boto/boto/issues/840
+        # headers['Expect'] = '100-Continue'
         headers = boto.utils.merge_meta(headers, self.metadata, provider)
         resp = self.bucket.connection.make_request(
             'PUT',
